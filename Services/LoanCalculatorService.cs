638267cf32b84702a32eb77207ebce22f7561ed0
Sempyi <167787212+Sempyi@users.noreply.github.com>
2024-10-14 20:14:32 +0500

    Update LoanCalculatorService.cs

--- Services/LoanCalculatorService.cs
+++ Services/LoanCalculatorService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using LoanCalculator.Models;
 
@@ -6,21 +6,27 @@ namespace LoanCalculator.Services
 {
     public class LoanCalculatorService
     {
+        /// <summary>
+        /// Сервис для расчёта. Сначала идёт расчёт месячной процентной ставки, затем аннуитетного коэфицента, затем цикл расчитывает каждый платёж.
+        /// </summary>
+        /// <param name="loanAmount"></param>
+        /// <param name="loanTerm"></param>
+        /// <param name="interestRate"></param>
+        /// <returns></returns>
         public LoanResultModel CalculateLoan(decimal loanAmount, int loanTerm, decimal interestRate)
         {
             var result = new LoanResultModel();
             result.Schedule = new List<PaymentScheduleItem>();
 
-            decimal monthlyRate = interestRate / 12 / 100;  // Месячная процентная ставка
+            decimal monthlyRate = interestRate / 12 / 100;  
             int totalMonths = loanTerm;
 
-            // Расчет аннуитетного коэффициента
             decimal annuityFactor = (monthlyRate * (decimal)Math.Pow((double)(1 + monthlyRate), totalMonths)) /
                                     (decimal)(Math.Pow((double)(1 + monthlyRate), totalMonths) - 1);
 
             result.MonthlyPayment = loanAmount * annuityFactor;
             decimal remainingBalance = loanAmount;
-
+            
             for (int month = 1; month <= totalMonths; month++)
             {
                 decimal interestPayment = remainingBalance * monthlyRate;
